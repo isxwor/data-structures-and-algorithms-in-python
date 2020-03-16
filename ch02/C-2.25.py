57db25691ffeb153899061d143ac6ecf6f4f4358
greyhere <57859708+greyhere@users.noreply.github.com>
2020-03-16 12:38:52 +0000

    refactor codes

--- ch02/C-2.25.py
+++ ch02/C-2.25.py
@@ -1,5 +1,10 @@
 ''' C-2.25
-Exercise R-2.12 uses the __mul__ method to support multiplying a Vector by a number, while Exercise R-2.14 uses the __mul__ method to support computing a dot product of two vectors. Give a single implementation of Vector.__mul__ that uses run-time type checking to support both syntaxes u * v and u * k, where u and v designate vector instances and k represents a number.
+Exercise R-2.12 uses the __mul__ method to support multiplying a Vector
+by a number, while Exercise R-2.14 uses the __mul__ method to support
+computing a dot product of two vectors. Give a single implementation of
+Vector.__mul__ that uses run-time type checking to support both syntaxes
+u * v and u * k, where u and v designate vector instances and k represents
+a number.
 '''
 
 class Vector:
--- ch02/C-2.29.py
+++ ch02/C-2.29.py
@@ -1,5 +1,9 @@
 ''' C-2.29
-Modify the PredatoryCreditCard class from Section 2.4.1 so that a customer is assigned a minimum monthly payment, as a percentage of the balance, and so that a late fee is assessed if the customer does not subsequently pay that minimum amount before the next monthly cycle.
+Modify the PredatoryCreditCard class from Section 2.4.1 so that
+a customer is assigned a minimum monthly payment, as a percentage
+of the balance, and so that a late fee is assessed if the customer
+does not subsequently pay that minimum amount before the next monthly
+cycle.
 '''
 
 
