96a09d4da828293520dc8d4eacf441fa575e139f
greyhere <57859708+greyhere@users.noreply.github.com>
2020-04-26 17:33:42 +0000

    add alternative solution

--- ch04/C-4.22.py
+++ ch04/C-4.22.py
@@ -3,6 +3,7 @@ Develop a nonrecursive implementation of the version of power from
 Code Fragment 4.12 that uses repeated squaring.
 '''
 
+# -- Solution one --
 def power(x, n):
     result = 1
     while n > 0:
@@ -13,8 +14,23 @@ def power(x, n):
         result *= x
     return result
 
+# -- Solution two --
+def _power(x, n):
+    factor = n
+    partial = 1
+    counter = 0
+    while factor:
+        factor = factor >> 1
+        counter += 1
+    while counter+1:
+        partial *= partial
+        if (n >> counter) & 1:
+            partial *= x
+        counter -= 1
+    return partial
+
 
 if __name__ == '__main__':
     xn = list(map(int, input('Enter base and exponent [space seperated]: ').split()))
-    test = power(*xn)
-    print(test)
+    print(power(*xn))
+    print(_power(*xn))
